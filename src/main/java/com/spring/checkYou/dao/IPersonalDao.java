701e6adcd85af8772b76cf04ebdf10bcf0236214
shrtorznzl <shrtorznzl@gmail.com>
2018-12-18 21:20:48 +0900

    interface comment added

--- src/main/java/com/spring/checkYou/dao/IPersonalDao.java
+++ src/main/java/com/spring/checkYou/dao/IPersonalDao.java
@@ -1,7 +1,7 @@
 package com.spring.checkYou.dao;
 
 import com.spring.checkYou.dto.WorkDto;
-
+// 개인 일일 시간관리 쿼리 인터페이스
 public interface IPersonalDao {
 	public void addNewWork(String id, String worktype, String workname, String workdetail);
 	public String checkWork(String workname);
